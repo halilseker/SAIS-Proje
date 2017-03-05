32aaf352556076ad51f911c2421578d60942b749
halilseker <g141210082@sakarya.edu.tr>
2017-03-05 21:51:03 +0300

    Sais Proje Toplantılarında Yaptığım çalışmaların özeti

---
+++ 04_03_17_U1/CMakeFiles/VideoDetectorum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VideoDetectorum.dir/VideoDetectorum.cpp.o"
+  "VideoDetectorum.pdb"
+  "VideoDetectorum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VideoDetectorum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
